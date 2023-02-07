5d8ec5956042cf435afdcd0b8747d6fce77b551e
Mykola <mykola.terianyk@drzewniak.slupsk.pl>
2023-02-07 13:51:24 +0100

    Stable beta 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,6 @@ class _MyHomePageState extends State<MyHomePage> {
       child: ElevatedButton(onPressed: (){
        calculation(btntxt);
       },
-
         child: Text(btntxt,
           style: TextStyle(
               fontSize: 40,
@@ -43,8 +42,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
 
         style: ElevatedButton.styleFrom(
+          shadowColor: Color(0xFFFFF),
           backgroundColor: btncolor,
           shape: CircleBorder(),
+
         ),
       ),
 
@@ -77,19 +78,78 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceAround ,
               children: [
                 SizedBox(width: 80, height: 80,
-                child: calcbutton('C', Colors.white70, Colors.red),
+                child: calcbutton('C', Colors.grey.shade100, Colors.red),
                 ),
                 SizedBox(width: 80, height: 80,
-                  child:calcbutton('( )', Colors.white70, Colors.green),
+                  child:calcbutton('( )', Colors.grey.shade100, Colors.green),
                 ),
                 SizedBox(width: 80, height: 80,
-                  child:calcbutton('%', Colors.white70, Colors.green),
+                  child:calcbutton('%', Colors.grey.shade100, Colors.green),
                 ),
                 SizedBox(width: 80, height: 80,
-                  child:calcbutton('/', Colors.white70, Colors.green),
+                  child:calcbutton('/', Colors.grey.shade100, Colors.green),
                 ),
 
 
+              ],
+            ),
+            const SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround ,
+              children: [
+                SizedBox(width: 80, height: 80,
+                  child: calcbutton('7', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('8', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('9', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('x', Colors.grey.shade100, Colors.green),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10,),
+            Row(
+
+              mainAxisAlignment: MainAxisAlignment.spaceAround ,
+              children: [
+                SizedBox(width: 80, height: 80,
+                  child: calcbutton('4', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('5', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('6', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('-', Colors.grey.shade100, Colors.green),
+                ),
+
+
+
+
+              ],
+            ),
+            const SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround ,
+              children: [
+                SizedBox(width: 80, height: 80,
+                  child: calcbutton('1', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('2', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('3', Colors.grey.shade100, Colors.black),
+                ),
+                SizedBox(width: 80, height: 80,
+                  child:calcbutton('+', Colors.grey.shade100, Colors.green),
+                ),
               ],
             ),
             SizedBox(height: 10,),
@@ -97,72 +157,13 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceAround ,
               children: [
                 SizedBox(width: 80, height: 80,
-                  child: calcbutton('7', Colors.white70, Colors.black),
+                  child: calcbutton('+/-', Colors.grey.shade100, Colors.black),
                 ),
                 SizedBox(width: 80, height: 80,
-                  child:calcbutton('8', Colors.white70, Colors.black),
+                  child:calcbutton('0', Colors.grey.shade100, Colors.black),
                 ),
                 SizedBox(width: 80, height: 80,
-                  child:calcbutton('9', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child:calcbutton('x', Colors.white70, Colors.green),
-                ),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Row(
-
-              mainAxisAlignment: MainAxisAlignment.spaceAround ,
-              children: [
-                SizedBox(width: 80, height: 80,
-                  child: calcbutton('4', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child:calcbutton('5', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child:calcbutton('6', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child:calcbutton('-', Colors.white70, Colors.green),
-                ),
-
-
-
-
-              ],
-            ),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround ,
-              children: [
-                SizedBox(width: 80, height: 80,
-                  child: calcbutton('1', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child:calcbutton('2', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child:calcbutton('3', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child:calcbutton('+', Colors.white70, Colors.green),
-                ),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround ,
-              children: [
-                SizedBox(width: 80, height: 80,
-                  child: calcbutton('+/-', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child:calcbutton('0', Colors.white70, Colors.black),
-                ),
-                SizedBox(width: 80, height: 80,
-                  child: calcbutton(',', Colors.white70, Colors.black),
+                  child: calcbutton(',', Colors.grey.shade100, Colors.black),
                 ),
                 SizedBox(width: 80, height: 80,
                   child:calcbutton('=', Colors.green, Colors.white),
@@ -190,14 +191,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
     if(btnText  == 'C') {
-      text ='0';
-      numOne = 0;
-      numTwo = 0;
-      result = '';
-      finalResult = '0';
-      opr = '';
-      preOpr = '';
+      if(text == '0'){
 
+      }else {
+        text = '';
+        numOne = 0;
+        numTwo = 0;
+        result = '';
+        finalResult = '0';
+        opr = '';
+        preOpr = '';
+      }
     } else if( opr == '=' && btnText == '=') {
 
       if(preOpr == '+') {
@@ -218,6 +222,7 @@ class _MyHomePageState extends State<MyHomePage> {
         numTwo = double.parse(result);
       }
 
+
       if(opr == '+') {
         finalResult = add();
       } else if( opr == '-') {
@@ -234,22 +239,31 @@ class _MyHomePageState extends State<MyHomePage> {
     else if(btnText == '%') {
       result = numOne / 100;
       finalResult = doesContainDecimal(result);
-    } else if(btnText == '.') {
-      if(!result.toString().contains('.')) {
-        result = result.toString()+'.';
+    } else if(btnText == ',') {
+      if(!result.toString().contains(',')) {
+        if(finalResult == '0'){
+          result = result.toString()+'0,';
+        }else{
+        result = result.toString()+',';
+        }
       }
       finalResult = result;
     }
-
     else if(btnText == '+/-') {
+      if(text == '0'){
+
+      }else{
       result.toString().startsWith('-') ? result = result.toString().substring(1): result = '-'+result.toString();
-      finalResult = result;
+      finalResult = result;}
 
     }
-
     else {
-      result = result + btnText;
-      finalResult = result;
+      if(text == '0' && btnText == '0'){
+
+      }else {
+        result = result + btnText;
+        finalResult = result;
+      }
     }
 
 
@@ -258,8 +272,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
 
   }
-
-
   String add() {
     result = (numOne + numTwo).toString();
     numOne = double.parse(result);
@@ -285,12 +297,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   String doesContainDecimal(dynamic result) {
 
-    if(result.toString().contains('.')) {
-      List<String> splitDecimal = result.toString().split('.');
-      if(!(int.parse(splitDecimal[1]) > 0))
+    if(result.toString().contains(',')) {
+      List<String> splitDecimal = result.toString().split(',');
+      if(!(int.parse(splitDecimal[1]) > 0)) {
         return result = splitDecimal[0].toString();
+      }
     }
     return result;
   }
+
 }
+
 
